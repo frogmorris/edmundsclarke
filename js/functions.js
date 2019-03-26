3c6a421f92bbdbbe29375a46efd6ddfb7b340550
frogmorris <frogmorris@frogmorris.net>
2019-03-26 13:48:04 +0000

    Updates moved to end of file
    for ease of comparison

--- js/functions.js
+++ js/functions.js
@@ -47,3 +47,53 @@ $(window).scroll(function () {
 
         }
       }); 
+
+
+
+
+jQuery( document ).ready(function($) {
+    
+  var allPanels = $('.toggle-content').hide();
+    
+  $('.toggle-title').click(function() {
+    allPanels.slideUp();
+       $this = $(this);
+      $target =  $this.next();
+      
+    
+      if($target.hasClass('active')){
+        $target.removeClass('active').slideUp(); 
+        $this.removeClass('active');   
+      }else{
+        allPanels.prev().removeClass('active');
+        allPanels.removeClass('active').slideUp();
+        $target.addClass('active').slideDown();
+        $this.addClass('active'); 
+      }
+      
+    return false;
+  });  
+
+    var alltogglelist = $('.togglelist-content');  
+        var alltogglelisttitle = $('.togglelist-title');  
+  $('.togglelist-title').click(function() {
+      $this = $(this);
+      $target =  $this.next();
+        alltogglelist.removeClass('active').slideUp();  
+         alltogglelisttitle.removeClass('active');   
+      if($target.hasClass('active')){
+        $target.removeClass('active').slideUp(); 
+        $this.removeClass('active');   
+      }else{
+        allPanels.prev().removeClass('active');
+        allPanels.removeClass('active').slideUp();
+        $target.addClass('active').slideDown();
+        $this.addClass('active'); 
+      }
+      
+    return false;      
+      
+            
+  });
+      
+  });
