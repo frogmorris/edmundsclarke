ddc7a4dc48253e8659cbd0f4c33e3076565878d3
frogmorris <frogmorris@frogmorris.net>
2019-03-26 13:05:55 +0000

    19 March updates

--- js/functions.js
+++ js/functions.js
@@ -1,37 +1,51 @@
-window.onload = function() {
- $('.matchHeight').matchHeight();
- $('.woocommerce-LoopProduct-link').matchHeight();    
-}
-
-
-
-    $("li.menu-item-has-children > a").append( "<span class='mobdrop'></span>" );
-	$("li.menu-item-has-children a span.mobdrop").click(function() {
-    $(this).parent().parent().siblings().removeClass( "show-sub-menu" );
-    $(this).parent().parent().toggleClass( "show-sub-menu" );
-    return false;
-	});
-
-	$("#menu-toggle").click(function() {
-    $(this).toggleClass("close-menu");   
-    $(".main-navigation ul#primary-menu").toggleClass('open-menu');    
-        
-    })
-
-	$(".open-search>a").click(function() {
-    $("#search-header").slideToggle( "slow"); 
-    })
-
-	$("#close-search").click(function() {
-    $("#search-header").slideUp( "slow"); 
-    })
-
-	$("#show-filters").click(function() {
-    $(this).toggleClass( "show-filters-open" );    
     $("#display-filters").slideToggle( "slow"); 
     })
 
+jQuery( document ).ready(function($) {
+    
+  var allPanels = $('.toggle-content').hide();
+    
+  $('.toggle-title').click(function() {
+    allPanels.slideUp();
+       $this = $(this);
+      $target =  $this.next();
+      
+    
+      if($target.hasClass('active')){
+        $target.removeClass('active').slideUp(); 
+        $this.removeClass('active');   
+      }else{
+        allPanels.prev().removeClass('active');
+        allPanels.removeClass('active').slideUp();
+        $target.addClass('active').slideDown();
+        $this.addClass('active'); 
+      }
+      
+    return false;
+  });
+	
+	
+	    var alltogglelist = $('.togglelist-content');  
+        var alltogglelisttitle = $('.togglelist-title');  
+  $('.togglelist-title').click(function() {
+      $this = $(this);
+      $target =  $this.next();
+        alltogglelist.removeClass('active').slideUp();  
+         alltogglelisttitle.removeClass('active');   
+      if($target.hasClass('active')){
+        $target.removeClass('active').slideUp(); 
+        $this.removeClass('active');   
+      }else{
+        $target.addClass('active').slideDown();
+        $this.addClass('active'); 
+      }
+      
+    return false;      
+      
+            
+  });
 
+});
 
 
 
